80527c87d3c2afac6fd330bd084d0e7702ac1e08
Leograzioli <leograzioli@gmail.com>
2022-10-27 14:10:55 +0200

    implementato add object in array

--- js/script.js
+++ js/script.js
@@ -3,7 +3,14 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            
+            todoUser: "",
+            todoList: []
+        }
+    },
+    methods: {
+        addTodo() {
+            this.todoList.push({text:this.todoUser, done: false})
+            this.todoUser = ""
         }
     }
 }).mount("#app")
