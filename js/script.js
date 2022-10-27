1f0c2fa190d5946cba24b6357fb01712578da8aa
Leograzioli <leograzioli@gmail.com>
2022-10-27 14:52:37 +0200

    comments

--- js/script.js
+++ js/script.js
@@ -8,15 +8,20 @@ createApp({
         }
     },
     methods: {
+
+        //add to-do imputed by user on to-so object list
         addTodo() {
             if (this.todoUser !== "")
             this.todoList.push({text:this.todoUser, done: false})
             this.todoUser = ""
         },
+
+        //change the status of done key from the object inside todoList
         taskDone(index) {
            this.todoList[index].done = !this.todoList[index].done
         },
         
+        //remove the object from todoList in the clicked item index
         cancelTask(index) {
             this.todoList.splice([index], 1)
         }
